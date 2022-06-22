e97bb0511be633ed1409873a5d556b0f84001703
TalipSalihoglu <salihoglutalip@gmail.com>
2022-06-22 17:14:33 +0300

    add GenericRepository

---
+++ Core/DataAccess/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    internal class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity> 
+                                                        where TEntity : class, IEntity, new()
+                                                        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DataAccess/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IGenericRepository<T> where T : IEntity,new()
+    {
+        T Get(Expression<Func<T,bool>> filter);
+        IList<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
