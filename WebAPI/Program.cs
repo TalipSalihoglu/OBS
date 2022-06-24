ad8aa751c777f5994c5630d314d2c188ec203032
TalipSalihoglu <salihoglutalip@gmail.com>
2022-06-24 17:39:49 +0300

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,20 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+  options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IStudentService, StudentManager>();
+builder.Services.AddSingleton<IStudentDal, StudentDal>();
 
 var app = builder.Build();
 
