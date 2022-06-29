a0cfdacc74a44aec15d74ad7ef9775311e2b903c
TalipSalihoglu <salihoglutalip@gmail.com>
2022-06-29 16:13:05 +0300

    add ExamManager

---
+++ Business/Abstract/IExamService.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Entities.Dtos.ExamDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IExamService
+    {
+        Exam Get(Expression<Func<Exam, bool>> filter);
+        IList<Exam> GetList(Expression<Func<Exam, bool>> filter = null);
+        IList<Exam> GetAllExamByStudentId(int studentId);
+        IList<Exam> GetAllExamByCourseId(int courseId);
+        IList<Exam> GetExamByStudentIdAndCourseId(int studentId, int CourseId);
+        void Add(Exam exam);
+        void Update(Exam exam);
+        void Delete(Exam exam);
+        
+
+    }
+}
---
+++ Business/Concrete/ExamManager.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dtos.ExamDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ExamManager : IExamService
+    {
+        private readonly IExamDal _examDal;
+        public ExamManager(IExamDal examDal)
+        {
+            _examDal = examDal;
+        }
+
+        public void Add(Exam exam)
+        {
+            _examDal.Add(exam);
+        }
+
+        public void Delete(Exam exam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Exam> GetAllExamByStudentId(int studentId)
+        {
+            return _examDal.GetList(x => x.StudentId == studentId);
+
+        }
+        public IList<Exam> GetAllExamByCourseId(int courseId)
+        {
+            return _examDal.GetList(x => x.CourseId == courseId);
+        }
+        public IList<Exam> GetExamByStudentIdAndCourseId(int studentId,int CourseId)
+        {
+            return _examDal.GetList(x => x.StudentId == studentId && x.CourseId == CourseId);
+        }
+        public void Update(Exam exam)
+        {
+            _examDal.Update(exam);
+        }
+
+        public Exam Get(Expression<Func<Exam, bool>> filter)
+        {
+            return _examDal.Get(filter);
+        }
+
+        public IList<Exam> GetList(Expression<Func<Exam, bool>> filter = null)
+        {
+            return _examDal.GetList(filter);
+        }
+      
+    }
+}
