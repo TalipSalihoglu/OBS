9a775e6f0324be41e5e8cbb0dcf9bc08fdf89efc
TalipSalihoglu <salihoglutalip@gmail.com>
2022-06-30 16:17:55 +0300

    Create StudentCourse.cs

---
+++ Entities/Concrete/StudentCourse.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class StudentCourse : IEntity
+    {
+        public int Id { get; set; }
+        public int? StudentId { get; set; }
+        public int? CourseId { get; set; }
+        public Course? Course { get; set; }
+        public Student? Student { get; set; }
+    }
+}
