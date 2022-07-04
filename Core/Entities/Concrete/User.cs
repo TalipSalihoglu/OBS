7580daa926ec609e4d29ec39d99ec5ba252f3353
TalipSalihoglu <salihoglutalip@gmail.com>
2022-07-04 12:22:06 +0300

    add User and Role

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpireDate { get; set; }
+    }
+}
